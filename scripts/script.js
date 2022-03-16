0e0854a8b0bccc9aff8183d747d2f7a8c1e2bc5a
Davi Marques <davimarquesgar@gmail.com>
2022-03-15 21:56:34 -0300

    Canvas centr. no meio  de acordo com a tela

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
 // Objetos do jogo
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+
+Width = canvas.width = 700; // 700
+Height = canvas.height = 550; // 550
+
+let margem = (window.innerHeight - Height) / 2;
+canvas.style.marginTop = `${margem}px`;
