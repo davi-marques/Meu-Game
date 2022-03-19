20c755ea3417154535627bab2b6417180b3e08fd
Davi Marques <davimarquesgar@gmail.com>
2022-03-19 13:00:35 -0300

    Função de colisão

--- scripts/solid.js
+++ scripts/solid.js
@@ -9,3 +9,36 @@ class Blocks {
     }
 }
 
+
+// Bloco solido
+
+function blockSolid(r1, r2){
+    // r1 = personagem   r2 = bloco
+
+    // Catetos
+    let catX = (r1.posX + r1.width / 2) - (r2.posX + (r2.width / 2)-13);
+    let catY = (r1.posY + r1.height / 2) - (r2.posY + r2.height / 2);
+
+    // Soma das metades
+    let sumHalfWidth = (r1.width / 2) + (r2.width / 2);
+    let sumHalfHeight = (r1.height / 2) + (r2.height / 2);
+
+    if (Math.abs(catX) < sumHalfWidth && Math.abs(catY) < sumHalfHeight) {
+        let overlapX = sumHalfWidth - Math.abs(catX);
+        let overlapY = sumHalfWidth - Math.abs(catY);
+
+        if(overlapX >= overlapY) {
+            if(catY > 0) {
+                r1.posY += overlapY;
+            } else {
+                r1.posY -= overlapY;
+            }
+        } else {
+            if (catX > 0) {
+                r1.posX += overlapX;
+            } else {
+                r1.posX -= overlapX;
+            }
+        }
+    }
+}
