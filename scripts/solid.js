bc40655c6d5486d8d7b1d16a604bb5dd522070f1
Davi Marques <davimarquesgar@gmail.com>
2022-03-26 12:03:38 -0300

    Função de bloco sólido melhorada

--- scripts/solid.js
+++ scripts/solid.js
@@ -10,35 +10,19 @@ class Blocks {
 }
 
 
-// Bloco solido
+    solid() {
+        if (man.pos.y + man.height + man.speed.y >= this.pos.y && // Cima
+             man.pos.y + man.height + man.speed.y <= this.pos.y + this.height && // Baixo
+             man.pos.x+21 <= this.pos.x + this.width && // Direita
+             man.pos.x + man.width-21 >= this.pos.x) { // Esquerda
 
-function blockSolid(r1, r2){
-    // r1 = personagem   r2 = bloco
-
-    // Catetos
-    let catX = (r1.posX + r1.width / 2) - (r2.posX + (r2.width / 2)-13);
-    let catY = (r1.posY + r1.height / 2) - (r2.posY + r2.height / 2);
-
-    // Soma das metades
-    let sumHalfWidth = (r1.width / 2) + (r2.width / 2);
-    let sumHalfHeight = (r1.height / 2) + (r2.height / 2);
-
-    if (Math.abs(catX) < sumHalfWidth && Math.abs(catY) < sumHalfHeight) {
-        let overlapX = sumHalfWidth - Math.abs(catX);
-        let overlapY = sumHalfWidth - Math.abs(catY);
-
-        if(overlapX >= overlapY) {
-            if(catY > 0) {
-                r1.posY += overlapY;
-            } else {
-                r1.posY -= overlapY;
-            }
-        } else {
-            if (catX > 0) {
-                r1.posX += overlapX;
-            } else {
-                r1.posX -= overlapX;
-            }
+            man.speed.y = 0;                     // Para em cima do plataforma
+            man.pos.y = this.pos.y - man.height     // Aterisagem
+            pulos = 0;                           // Zera o contador de pulos para pular mais vezes
         }
     }
 }
+
+
+
+//         man.speed.y = 0;
